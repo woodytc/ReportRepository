5d32b3c314c9b885210ced097bbe1dd7b5aaa675
pomnot <pomnot@gmail.com>
2014-08-22 11:33:52 +0700

    Full Version

---
+++ KTBLeasing.Report.extjs/Models/User.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace Creating_a_custom_user_login_form.Models
+{
+    public class User
+    {
+        [Required]
+        [Display(Name = "User name")]
+        public string UserName { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+
+        [Display(Name = "Remember on this computer")]
+        public bool RememberMe { get; set; }
+    }
+}
